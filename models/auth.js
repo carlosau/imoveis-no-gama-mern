0cecc5e3e178313586cac67d044b3c23932bcce3
carlosau <carlosaugsz@hotmail.com>
2023-08-29 11:25:56 -0300

    created auth model

--- models/auth.js
+++ models/auth.js
@@ -1,8 +1,18 @@
 import {model, Schema, ObjectId} from 'mongoose';
 
 const schema = new Schema({
-    name: { type: String, required: true, trim: true },
-
-})
+    username: { type: String, required: true, trim: true, unique: true, lowercase: true },
+    name: { type: String, trim: true, default: '' },
+    email: { type: String, required: true, trim: true, unique: true, lowercase: true },
+    password: { type: String, required: true, maxLength: 128 },
+    address: { type: String, default: '' },
+    company: { type: String, default: '' },
+    whatsapp: { type: String, default: '' },
+    photo: {},
+    role: { type: [String], default: ["Buyer"], enum: ["Admin", "Buyer", "Seller"] },
+    enquiredProperties: [{ type: ObjectId, ref: 'Ad' }],
+    wishList: [{ type: ObjectId, ref: 'Ad' }],
+    resetCode: ""
+}, {timestamps: true})
 
 export default model('User', schema);
